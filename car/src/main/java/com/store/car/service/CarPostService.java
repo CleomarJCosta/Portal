de3e0cde1bcab62cd050a2e39faf32b78ebdcf47
CleomarJCosta <66894299+CleomarJCosta@users.noreply.github.com>
2023-08-21 21:22:02 -0300

    create CarPostServiceImpl

--- car/src/main/java/com/store/car/service/CarPostService.java
+++ car/src/main/java/com/store/car/service/CarPostService.java
@@ -1,4 +1,13 @@
 package com.store.car.service;
 
+import com.store.car.dto.CarPostDTO;
+
+import java.util.List;
+
 public interface CarPostService {
+    void newPostDetails(CarPostDTO carPostDTO);
+    List<CarPostDTO> getCarSales();
+    void changeCarSales(CarPostDTO carPostDTO, Long id);
+
+    void removeCarSale(Long postId);
 }
