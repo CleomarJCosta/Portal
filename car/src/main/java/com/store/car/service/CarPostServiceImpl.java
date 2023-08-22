28cef7a43489df42c167e9b43cc48130b413e85e
CleomarJCosta <66894299+CleomarJCosta@users.noreply.github.com>
2023-08-21 23:13:32 -0300

    update
    CarPostServiceImpl and CarPostDTO updated

--- car/src/main/java/com/store/car/service/CarPostServiceImpl.java
+++ car/src/main/java/com/store/car/service/CarPostServiceImpl.java
@@ -1,12 +1,43 @@
 package com.store.car.service;
 
 import com.store.car.dto.CarPostDTO;
+import com.store.car.entity.CarPostEntity;
+import com.store.car.repository.CarPostRepository;
+import com.store.car.repository.OwnerPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
-
+@Service
 public class CarPostServiceImpl implements CarPostService{
+    @Autowired
+    private CarPostRepository carPostRepository;
+
+    private OwnerPostRepository ownerPostRepository;
     @Override
     public void newPostDetails(CarPostDTO carPostDTO) {
+        CarPostEntity carPostEntity = mapCarDtoToEntity(carPostDTO);
+        carPostRepository.save(carPostEntity);
+    }
+
+    private CarPostEntity mapCarDtoToEntity(CarPostDTO carPostDTO) {
+        CarPostEntity carPostEntity = new CarPostEntity();
+        ownerPostRepository.findById(carPostDTO.getOwnerId()).ifPresentOrElse(item->{
+            carPostEntity.setOwnerPost(item);
+            carPostEntity.setContact(carPostDTO.getContact());
+        }, ()->{
+            throw new RuntimeException();
+        });
+        carPostEntity.setModel(carPostDTO.getModel());
+        carPostEntity.setBrand(carPostDTO.getBrand());
+        carPostEntity.setCity(carPostDTO.getCity());
+        carPostEntity.setCreatedDate(String.valueOf(new Date()));
+        carPostEntity.setDescription(carPostDTO.getDescription());
+        carPostEntity.setEngineVersion(carPostDTO.getEngineVersion());
+        carPostEntity.setPrice(carPostDTO.getPrice());
+
+        return carPostEntity;
 
     }
 
---
+++ car/src/main/java/com/store/car/service/OwnerPostService.java
@@ -0,0 +1,7 @@
+package com.store.car.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface OwnerPostService {
+}
