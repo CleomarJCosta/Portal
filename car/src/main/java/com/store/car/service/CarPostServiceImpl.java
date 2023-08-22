5dd5ef8134bca51b07546035828796302cc42e6e
CleomarJCosta <66894299+CleomarJCosta@users.noreply.github.com>
2023-08-21 21:40:29 -0300

    Repositories created

--- car/src/main/java/com/store/car/service/CarPostServiceImpl.java
+++ car/src/main/java/com/store/car/service/CarPostServiceImpl.java
@@ -1,4 +1,27 @@
 package com.store.car.service;
 
-public class CarPostServiceImpl {
+import com.store.car.dto.CarPostDTO;
+
+import java.util.List;
+
+public class CarPostServiceImpl implements CarPostService{
+    @Override
+    public void newPostDetails(CarPostDTO carPostDTO) {
+
+    }
+
+    @Override
+    public List<CarPostDTO> getCarSales() {
+        return null;
+    }
+
+    @Override
+    public void changeCarSales(CarPostDTO carPostDTO, Long id) {
+
+    }
+
+    @Override
+    public void removeCarSale(Long postId) {
+
+    }
 }
