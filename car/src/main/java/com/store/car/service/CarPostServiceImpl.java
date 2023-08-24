226adada9f8679718ec219d24e71121679f3d703
CleomarJCosta <cleomarcosta20@gmail.com>
2023-08-24 01:58:36 -0300

    OWnerPostServiceImpl created.

--- car/src/main/java/com/store/car/service/CarPostServiceImpl.java
+++ car/src/main/java/com/store/car/service/CarPostServiceImpl.java
@@ -7,13 +7,16 @@ import com.store.car.repository.OwnerPostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service
 public class CarPostServiceImpl implements CarPostService{
     @Autowired
     private CarPostRepository carPostRepository;
-
+    @Autowired
     private OwnerPostRepository ownerPostRepository;
     @Override
     public void newPostDetails(CarPostDTO carPostDTO) {
@@ -43,16 +46,46 @@ public class CarPostServiceImpl implements CarPostService{
 
     @Override
     public List<CarPostDTO> getCarSales() {
-        return null;
+       List<CarPostDTO> listCarsSales = new ArrayList<>();
+       carPostRepository.findAll().forEach(item->{
+           listCarsSales.add(mapCarEntityToDTO(item));
+       });
+       return listCarsSales;
     }
 
     @Override
     public void changeCarSales(CarPostDTO carPostDTO, Long id) {
+        carPostRepository.findById(id).ifPresentOrElse(item->{
+            item.setDescription(carPostDTO.getDescription());
+            item.setContact(carPostDTO.getContact());
+            item.setPrice(carPostDTO.getPrice());
+            item.setBrand(carPostDTO.getBrand());
+            item.setEngineVersion(carPostDTO.getEngineVersion());
+            item.setModel(carPostDTO.getModel());
+
+            carPostRepository.save(item);
+
+        }, ()->{
+            throw new NoSuchElementException();
+        });
 
     }
 
     @Override
     public void removeCarSale(Long postId) {
+        carPostRepository.deleteById(postId);
+    }
+
+    private CarPostDTO mapCarEntityToDTO(CarPostEntity carPostEntity){
+        return CarPostDTO.builder()
+                .brand(carPostEntity.getBrand())
+                .city(carPostEntity.getCity())
+                .model(carPostEntity.getModel())
+                .description(carPostEntity.getDescription())
+                .engineVersion(carPostEntity.getEngineVersion())
+                .createdDate(carPostEntity.getCreatedDate())
+                .ownerName(carPostEntity.getOwnerPost().getName())
+                .price(carPostEntity.getPrice()).build();
 
     }
 }
---
+++ car/src/main/java/com/store/car/service/OWnerPostServiceImpl.java
@@ -0,0 +1,12 @@
+package com.store.car.service;
+
+import com.store.car.dto.OwnerPostDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OWnerPostServiceImpl implements OwnerPostService {
+    @Override
+    public void createOwnerPost(OwnerPostDTO ownerPostDTO) {
+
+    }
+}
